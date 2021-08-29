87230cd8b979d1e94ecf265cbdea43ae284bc13c
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-08-29 13:50:00 +0600

    filtering jobs by required skill

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+print('What skills are you not familiar with? ')
+unfamiliar_skill = input(":")
+print(f"Filtering out {unfamiliar_skill}")
+
 html_res = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
 soup = BeautifulSoup(html_res, 'lxml')
@@ -12,8 +16,15 @@ for job in jobs:
         company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
         skills = job.find('span', class_='srp-skills').text.replace(' ', '')
         links = job.header.h2.a['href']
-        print(f"Company Name: {company_name.strip()}")
-        print(f"Required skills: {skills.strip()}")
-        print(f"More Info: {links}")
+        if unfamiliar_skill not in skills:
+            print('')
+            print('')
+            
+            print(f"Company Name: {company_name.strip()}")
+            print('')
+            print(f"Required skills: {skills.strip()}")
+            print('')
+            print(f"More Info: {links}")
 
-        print('')
+            print('')
+            print('')
